a150f726cdf4c7f482fcd3cb52c52d54fda2b1bf
Carlos Custodio <chcustodio97@gmail.com>
2022-12-29 10:13:23 -0300

    Initial Commit

---
+++ fooD/fooDApp.swift
@@ -0,0 +1,17 @@
+//
+//  fooDApp.swift
+//  fooD
+//
+//  Created by Carlos Henrique on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct fooDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
